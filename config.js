c3acdd29ae54627a8f469190ddd1e5d45637b3b5
phyzical <5182053+phyzical@users.noreply.github.com>
2023-10-10 12:38:48 +0800

    add a config
    main

---
+++ config.js
@@ -0,0 +1,46 @@
+module.exports = {
+    regexManagers: [
+        {
+            "description": "Update Kubernetes version for Amazon EKS",
+            "fileMatch": [".+\\.tf$"],
+            "matchStrings": [
+                "\\s*#\\s*renovate:\\s*datasource=endoflife-date\\s*depName=(?<depName>.*?)( versioning=(?<versioning>.*?))?\\s.*?_version\\s*=\\s*\"(?<currentValue>.*)\""
+            ],
+            "datasourceTemplate": "endoflife-date",
+            "versioningTemplate": "{{#if versioning}}{{{versioning}}}{{/if}}"
+        },
+        {
+            "description": "Update dockerfile github releases",
+            "fileMatch": [
+                "Dockerfile"
+            ],
+            "datasourceTemplate": "github-releases",
+            "matchStrings": [
+                "\\s*#\\s*renovate:\\s*datasource=github-releases\\s*depName=(?<depName>.*?)\\s*ARG\\s.*?_VERSION\\s*=\\s*\"*(?<currentValue>.*)\"*"
+            ]
+        },
+        {
+            "fileMatch": ["Dockerfile"],
+            "matchStrings": [
+                "\\s*#\\s*renovate:\\s*datasource=yum\\s*repo=(?<registryUrl>[^\\s]+)\\s+(?<depName>[^\\s]+)-(?<currentValue>[^\\s-]+-[^\\s-]+)"
+            ],
+            "datasourceTemplate": "npm",
+            "versioningTemplate": "loose",
+            "registryUrlTemplate": "https://yum2npm.io/repos/{{replace '/' '/modules/' registryUrl}}/packages"
+        }
+    ],
+    packageRules: [{
+        "matchPackageNames": ["cost-analyzer"],
+        "sourceUrl": "https://github.com/kubecost/cost-analyzer-helm-chart",
+        "registryUrl": "https://kubecost.github.io/cost-analyzer",
+    }, {
+        "matchDatasources": ["endoflife-date"],
+        "matchPackageNames": ["amazon-eks"],
+        "extractVersion": "^(?<version>.*)-eks.+$"
+    },
+    {
+        "matchDatasources": ["github-releases"],
+        "extractVersion": "^v(?<version>.*)$"
+    }
+    ]
+};
