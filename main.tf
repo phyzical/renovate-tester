0f3b18a6b2ee43d34d16d91ce23186f55c5e84eb
phyzical <5182053+phyzical@users.noreply.github.com>
2024-06-25 13:42:57 +0800

    forgot version

--- main.tf
+++ main.tf
@@ -28,5 +28,5 @@ resource "aws_elasticache_replication_group" "redis_replication_group" {
 }
 
 
-# renovate:amiFilter=[{"Name":"engine","Values":["aurora-mysql"]},{"Name":"engine-version","Values":["8.0"]}]
+# renovate:amiFilter=[{"Name":"engine","Values":["aurora-mysql"]},{"Name":"engine-version","Values":["8.0"]}] depName=engineVersion
 engine_version = "8.0.mysql_aurora.3.05.0"
